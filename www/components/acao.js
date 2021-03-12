3e0b6923adb9b962b451527352923a33e6ff4640
Jonathan687 <jonathanpaulinosantana@gmail.com>
2021-03-13 07:56:32 +0900

    criei outros botões para conseguir fazer outros calculos.

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,16 @@
 // This is a JavaScript file
 
+$(document).on("click","#somar",function()[
+  var valor1 = $("#valor1").var();
+  var valor2 = $("#valor2").var();
+
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+  var resultado = parseFloat(valor1) - parseFloat(valor2);
+
+  var resultado = parseFloat(valor1) * parseFloat(valor2);
+]
+  var resultado = parseFloat(valor1) / parseFloat(valor2);
+  
+  $("#resultado").val(resultado);
+]);
