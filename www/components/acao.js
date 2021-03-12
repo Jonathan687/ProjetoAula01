d1d12ea14e7c8b05948d29fe9368de029f3207ea
Jonathan687 <jonathanpaulinosantana@gmail.com>
2021-03-13 08:22:43 +0900

    Correção dos erros dos calculos\; subtrair\; multiplicar\; dividir.

--- www/components/acao.js
+++ www/components/acao.js
@@ -3,14 +3,27 @@
 $(document).on("click","#somar",function()[
   var valor1 = $("#valor1").var();
   var valor2 = $("#valor2").var();
-
   var resultado = parseFloat(valor1) + parseFloat(valor2);
+  $("#resultado").var(resultado);
+]);
 
+$(document).on("click","#subtrair",function(){
+  var valor1 = $("$valor1").var();
+  var valor2 = $("$valor2").var();
   var resultado = parseFloat(valor1) - parseFloat(valor2);
+  $("#resultado").var(resultado);
+]);
 
+$(document).on("click","#multiplicar",function(){
+  var valor1 = $("$valor1").var();
+  var valor2 = $("$valor2").var();
   var resultado = parseFloat(valor1) * parseFloat(valor2);
-]
+  $("resultado").var(resultado);
+]);
+
+$(document).on("click","#dividir",function(){
+  var valor1 = $("$valor1").var();
+  var valor2 = $("$valor2").var();
   var resultado = parseFloat(valor1) / parseFloat(valor2);
-  
-  $("#resultado").val(resultado);
+  $("#resultado").var(resultado);
 ]);
